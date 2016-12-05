91bd5c584f761232dee3f732f19b1249b7d70df7
David Wenzel <codenull@gmail.com>
2016-12-04 18:00:21 -0800

    Added terrain generator, connected terrain to camera. Changed shaders to allow for easier light adjustment, Made mountains.html actually show some mountains.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 #ReverendCode.github.io
+
+
+
+Terrain generation sources:
+
+http://www.playfuljs.com/realistic-terrain-in-130-lines/
+http://www-cs-students.stanford.edu/~amitp/game-programming/polygon-map-generation/
+
+and for how to get the damn thing onto the screen.
 http://danni-three.blogspot.com/2013/09/threejs-heightmaps.html
 
 
--- js/mountains.js
+++ js/mountains.js
@@ -1,22 +1,19 @@
 
 var scene, camera, renderer;
-var lightX = 0.0;
+var  sunlight;
 var rising = true;
 
 //TODO: remove this test object
 var obj = null;
 
-function init () {
+function init (draw) {
 	//set the scene
 	scene = new THREE.Scene();
-
 	var screenWidth = window.innerWidth, screenHeight = window.innerHeight;
 	var viewAngle = 45, aspect = screenWidth / screenHeight, near = 0.1, far = 1000;
 	camera = new THREE.PerspectiveCamera(viewAngle, aspect, near, far);
-	scene.add(camera);
-	camera.position.set(1,2,40);
+	// camera.position.set(100,100,80);
 	camera.lookAt(scene.position);
-
 	renderer = new THREE.WebGLRenderer( {
 		antialias: true,
 		alpha: true
@@ -27,39 +24,44 @@ function init () {
 	container.appendChild(renderer.domElement);
 
 	// obj = createTorus();
-	obj = CreateWorld();
+	obj = draw;
+	obj.position.set(0,-50,0);
 
-
-	scene.add(obj);
+	// obj.add(camera);
+	camera.add(obj);
+	scene.add(camera);
+	// scene.add(obj);
 	animate();
 }
 
-function CreateWorld() { // and he said, let there be light.
-	var geometry = new THREE.PlaneGeometry(	10, // size in X (units?)
-											10, // Y
-											9,	// Resolution in X (Number of subdivisions)
-											9);	// Resolution in Y
-	var material = applyMaterial();
 
-	var plane = new THREE.Mesh(geometry, material);
 
-	for (var i = 0; i < plane.geometry.vertices.length; i++) {
+function CreateWorld(detail = 5) { // and he said, let there be light.
+	
+	var xS = 63, yS = 63;
 
-		// In here you can adjust the z (and presumably the x, and y if you want, but we don't, for now)
-		// vertices are accessed sequentially, fire your generated data here.
-		plane.geometry.vertices[i].z = Math.random();
-	}
-
-	return plane;
+	return THREE.Terrain({
+    easing: THREE.Terrain.Linear, 
+    frequency: 2.5,
+    heightmap: THREE.Terrain.Fault, //input terrain generator here.
+    material: applyMaterial(),
+    maxHeight: 100,
+    minHeight: -100,
+    steps: 1,
+    useBufferGeometry: false,
+    xSegments: xS,
+    xSize: 1024,
+    ySegments: yS,
+    ySize: 1024,
+});
 }
 
 function applyMaterial() {
 	var uniforms = THREE.UniformsUtils.merge( 
 		[ {
-			step: { type: 'f', value: 5.0},
+			step: { type: 'f', value: 10.0},
 			divisor: { type: 'f', value: 6.0},
-			colorRed: {type: 'c', value: new THREE.Color(0x004300)},
-			lightDir: {value: new THREE.Vector3(0.0,9999.0,9999.0)} 
+			colorRed: {type: 'c', value: new THREE.Color(0x0060ff)}
 		}]);
 	
 	return new THREE.ShaderMaterial({
@@ -69,28 +71,17 @@ function applyMaterial() {
 	}); 
 }
 
-function createTorus() {
+
+function CreateTorus() {
 	var geometry = new THREE.TorusGeometry(8,3,64,64);
 	return new THREE.Mesh(geometry, applyMaterial());
 }
 
 function animate() {
 	requestAnimationFrame(animate);
-	obj.rotation.x += 0.005;
-	obj.rotation.y += 0.01;
-	var val = 0.01;
-	if (rising) {
-		lightX += val;
-		rising = lightX < 1.0;
-	} else {
-		lightX -= val;
-		rising = lightX <= -1.0;
-	}
-	// obj.material.uniforms.lightDir.value.x = lightX;
-	// obj.material.uniforms.lightDir.value.y = -lightX;
-
-
-	
+	// obj.rotation.x += 0.005;
+	camera.rotation.y += 0.01;
+	// camera.position.y += .001;
 
 	renderer.render(scene, camera);
 
